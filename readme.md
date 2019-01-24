96621f3c45cf942318d7a24a8e438479b88bd31c
sunhuacheng <sunhuacheng@meituan.com>
2019-01-24 17:03:51 +0800

    update: of from merge map scan

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 rxjs v6 的学习demo
+
+https://rxjs-cn.github.io/learn-rxjs-operators/operators/combination/merge.html
---
+++ src/demo_01.js
@@ -0,0 +1,25 @@
+/**
+ * 创建类的操作符
+ */
+
+import * as rxjs from 'rxjs'
+import { map, filter, scan, mapTo } from 'rxjs/operators'
+
+const Log = console.log
+
+const createHello = rxjs.Observable.create((observer) => {
+  observer.next('hello')
+  observer.next('world')
+})
+createHello.subscribe(x => Log(x))
+
+/**
+ * of 依次接受多个任意类型的值依此创建observable对象
+ */
+let ofSomeThing = rxjs.of({ name: 'Brian' }, [1, 2, 3], function hello () {
+  return 'Hello'
+})
+ofSomeThing.subscribe(x => Log(x))
+
+let testScan = rxjs.of(1, 2, 3)
+testScan.pipe(mapTo(2), scan((a, b) => a + b, 0)).subscribe(x => Log(x))
